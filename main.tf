8e95cbbba49c16a3914d5c3738d9bb8bc3d9aa0f
VIVESH TYAGI <vivesht@gmail.com>
2024-01-27 19:56:58 +0530

    inital code 4

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 resource "google_storage_bucket" "a-exp" {
   name                     = "stanley-no-public-access"
+  project                  = "vivesh-405513"
   location                 = "US"
   force_destroy            = true
   public_access_prevention = "enforced"
